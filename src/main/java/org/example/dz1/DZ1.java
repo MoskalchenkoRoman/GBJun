0384e011117e6db211c684f9102bd812f9913413
Роман <110688570+MoskalchenkoRoman@users.noreply.github.com>
2023-11-29 08:50:18 +0300

    dz1

--- src/main/java/org/example/dz1/DZ1.java
+++ src/main/java/org/example/dz1/DZ1.java
@@ -1,2 +1,23 @@
-package org.example.dz1;public class DZ1 {
+package org.example.dz1;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class DZ1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(8);
+        list.add(4);
+
+        String aver;
+
+        aver = list.stream().filter(chislo -> chislo % 2 == 0).mapToInt(Integer::intValue).average().toString();
+        System.out.println(aver);
+    }
 }
